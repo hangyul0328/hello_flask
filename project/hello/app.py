9ef4b708eca2da5f00300af83ce7a2cb0d62033e
hangyul <idkeng1357@gmail.com>
2026-04-07 13:57:12 +0900

    feat: 리스트와 for문으로 화면 자동 생성 구현

--- project/hello/app.py
+++ project/hello/app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # 이제는 JSON이 아니라 HTML 파일을 보냅니다
-    return render_template('index.html')
+    foods = ["치킨", "피자", "햄버거", "떡볶이", "라면", "초밥", "김밥"]
+    return render_template('index.html', data=foods)
 
 if __name__ == '__main__':
     app.run(debug=True)
